faeea9b1e461ec2dc7b18bfce5c89f5847c87800
Narayan Rai <narayanrai97@gmail.com>
2018-02-27 18:37:48 +0000

    I4: Colorizing the Output & Guess Limiting

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,30 +1,65 @@
+# Guess class created
+class Guess
+    @@count = 6
+    def initialize(guess)
+        @guess = guess
+        @@count -= 1
+    end
+    
+    def self.count
+        @@count
+    end
+    
+    def to_i
+        @guess
+    end
+end
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
+Guess.new(0)
+
+$SECRET_NUMBER = rand(100)
 
 get '/' do 
-    guess = params[:guess].to_i
+    guess = Guess.new(params[:guess].to_i)
     message = check_guess(guess)
-    erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+    erb :index, :locals => {:number => $SECRET_NUMBER, :message => message, 
+                            :color => @color, :guess => guess, :count => @@count-1}
 end
 
-def check_guess(x)
-    if x > SECRET_NUMBER
-        if x > SECRET_NUMBER + 5
-            "Way too high!"
+def check_guess(guess)
+    x = guess.to_i
+    if Guess.count 
+        if Guess.count == 0
+            @@count = 6
+            $SECRET_NUMBER = rand(100)
+            @color2 = "blue"
+            "You lost it! New SECRET NUMBER has been reset!!"
         else
-            "Too high!"
+            if x > $SECRET_NUMBER
+                if x > $SECRET_NUMBER + 5
+                    @color = "red"
+                    "Way too high!"
+                else
+                    @color ="yellow"
+                    "Too high!"
+                end
+            elsif x < $SECRET_NUMBER
+                if x < $SECRET_NUMBER - 5
+                    @color = "red"
+                    "Way too low!"
+                else
+                    @color = "yellow"
+                    "Too low!"
+                end
+            else
+                $SECRET_NUMBER = rand(100)
+                @@count = 6
+                @color = "green"
+                "You WON!"
+            end
         end
-    elsif x < SECRET_NUMBER
-        if x < SECRET_NUMBER - 5
-            "Way too low!"
-        else
-            "Too low!"
-        end
-    else
-        "You got it right!
-        The SECRET NUMBER is #{SECRET_NUMBER}"
     end
 end
 
